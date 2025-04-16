d34f0df4044885e82bfb8a25373016a005e23064
Maha6468 <motherma7633@gmail.com>
2025-04-16 23:20:42 +0600

    Row column

---
+++ lib/class_serial/row_column_5.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RowColumn extends StatelessWidget {
+  const RowColumn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('Row and Column'),
+        centerTitle: true,
+        ),
+      body:Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+        Text('flutter app'),
+        Text('ios android'),
+        Text('kotlin app')
+      ],
+      )
+    );
+  }
+}
