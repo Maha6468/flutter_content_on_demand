ab22fa6ebc4f4516fa6f4ab72c348891b311d4b9
Maha6468 <motherma7633@gmail.com>
2025-05-15 23:13:56 +0600

    Form,TextFormField,Validator

--- lib/practices/practices.dart
+++ lib/practices/practices.dart
@@ -8,13 +8,53 @@ class TextFromField extends StatefulWidget {
 }
 
 class _TextFromFieldState extends State<TextFromField> {
+  final _formKey=GlobalKey<FormState>();
+   TextEditingController _nameController=TextEditingController();
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text('TextFromField1'),
         centerTitle: true,
+      ),
+      body:Center(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+
+                validator: (value){
+                  if (value == null || value.isEmpty){
+                    return 'Name can not be empty';
+                  }
+                  else if(value.contains('@') || value.contains(']') || value.contains('[') ){
+                    return 'Enter a valid Name';
+                  }
+                  else{
+                    return null;
+                  }
+                },
+                controller: _nameController,
+                decoration: InputDecoration(
+                  labelText: 'Enter your name',
+                  hintText: 'Name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20)
+                  )
+                ),
+              ),
+              SizedBox(height: 5,),
+              ElevatedButton(onPressed: (){
+                if(_formKey.currentState!.validate()){
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Processiong Data'),));
+                }
+              }, child:Text('Submit'))
+            ],
+          ),
+        ),
       ),
     );
   }
