a00fb68580751a6829f3a15b59894e142e3a2826
Maha6468 <motherma7633@gmail.com>
2025-05-16 23:12:28 +0600

    GridView,

--- lib/practices/practices.dart
+++ lib/practices/practices.dart
@@ -10,34 +10,57 @@ class Gridview extends StatefulWidget {
 class _GridviewState extends State<Gridview> {
   @override
   Widget build(BuildContext context) {
+    var arrColor=[
+      Colors.deepPurpleAccent,
+      Colors.pink,
+      Colors.amber,
+      Colors.brown,
+      Colors.green,
+      Colors.grey,
+      Colors.purpleAccent,
+      Colors.indigo
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Text('GridView Practices'),
         centerTitle: true,
         backgroundColor: Colors.cyan,
       ),
-      body: GridView.count(
-        crossAxisCount:3,
-        children: [
-          Container(
-            color: Colors.amber,
-          ),
-          Container(
-            color: Colors.blue,
-          ),
-          Container(
-            color: Colors.blueGrey,
-          ),
-          Container(
-            color: Colors.deepPurpleAccent,
-          ),
-          Container(
-            color: Colors.pink,
-          ),
-          Container(
-            color: Colors.purpleAccent,
-          ),
-        ],
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        //color: Colors.teal,
+        child: GridView.count(
+          crossAxisCount:2,
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
+          children: [
+            Container(
+              color:arrColor[0],
+            ),
+            Container(
+              color:arrColor[1],
+            ),
+            Container(
+              color:arrColor[2],
+            ),
+            Container(
+              color:arrColor[3],
+            ),
+            Container(
+              color:arrColor[4],
+            ),
+            Container(
+              color:arrColor[5],
+            ),
+            Container(
+              color:arrColor[6],
+            ),
+            Container(
+              color:arrColor[7],
+            ),
+          ],
+        ),
       ),
     );
   }
